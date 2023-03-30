707c751d0ba3fe8363fbe3962cb1b8583a4c577f
yanglei <m.justin.yang@outlook.com>
2023-03-30 10:01:52 +0800

    docs： 添加文档

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,64 @@
+import menu from "./menu";
+
+module.exports = {
+  lang: "zh-CN",
+  title: "Vue-H5-Template",
+  base: "/vue-h5-template/",
+  description: "一个清新优雅的H5模版",
+  lastUpdated: true,
+  head: [
+    // 添加图标
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+  ],
+  themeConfig: {
+    logo: "/logo.svg",
+    nav: nav(),
+    sidebar: menu,
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/honghuangdc/soybean-admin" },
+      {
+        icon: "discord",
+        link: "https://qm.qq.com/cgi-bin/qm/qr?k=XTjkqjV1KynFoEAdrqxMd6i71-TtCVGV&jump_from=webapi",
+      },
+    ],
+    editLink: {
+      text: "为此页提供修改建议",
+      pattern:
+        "https://github.com/honghuangdc/soybean-admin-docs/tree/main/:path",
+    },
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2022-present Fonghehe",
+    },
+    algolia: {
+      appId: "CV2I8NGBKZ",
+      apiKey: "63c37a467485b00383daacf7340b4ca0",
+      indexName: "vue-h5-template",
+    },
+  },
+};
+
+function nav() {
+  return [
+    { text: "介绍", link: "/guide/" },
+    {
+      text: "相关链接",
+      items: [
+        {
+          text: "预览地址",
+          link: "https://soybean.pro",
+        },
+        {
+          text: "项目源码",
+          link: "https://github.com/honghuangdc/soybean-admin",
+        },
+        {
+          text: "文档源码",
+          link: "https://github.com/honghuangdc/soybean-admin-docs",
+        },
+      ],
+    },
+    { text: "加入我们", link: "/guide/about/" },
+  ];
+}
---
+++ docs/index.md
@@ -0,0 +1,35 @@
+---
+layout: home
+
+title: Vue-H5-Template
+titleTemplate: 一个清新优雅的H5模版
+
+hero:
+  name: Vue-H5-Template
+  text: 清新优雅的H5模版
+  tagline: 基于 Vue3 + Vite4 + TS + VantUI + Pinia
+  image:
+    src: ./logo.png
+    alt: dyyanewade
+  actions:
+    - theme: brand
+      text: 开始
+      link: /guide/
+    - theme: alt
+      text: Github仓库
+      link: https://github.com/sunniejs/vue-h5-template
+
+features:
+  - icon: 🆕
+    title: 最新流行技术栈
+    details: 基于Vue3、Vite4、TS、VantUI和Pinia等最新技术栈开发
+  - icon: 📦
+    title: 代码规范
+    details: 完整的eslint、prettier、stylelint规范，加上husky，帮助你更好的管理代码
+  - icon: 🛠️
+    title: 配置优化
+    details: 完整配置的vue脚手架插件和优化，让你专注于业务开发，更高效
+  - icon: ⚙️
+    title: More
+    details: 加入我们，更多能力等你挖掘....
+---
